5dcc0ebfd815e6641602bd72258d9da47c63c977
Ezra T Huffman <joshooha@gmail.com>
2018-09-20 20:55:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -61,7 +61,6 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
-  cart = {};
   if (cardNumber === undefined) {
     return "Sorry, we don't have a credit card on file for you.";
   } 
