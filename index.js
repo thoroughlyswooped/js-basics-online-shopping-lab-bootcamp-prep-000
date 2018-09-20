dd9682c5b04eff0c81a6c38945fcb94e8a75c540
Ezra T Huffman <joshooha@gmail.com>
2018-09-20 20:38:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -60,5 +60,5 @@ function placeOrder(cardNumber) {
   if (cardNumber === undefined) {
     return "Sorry, we dont't have a credit card on file for you.";
   } 
-  return `Your total cost is $${total()}, which will be charged to the ${cardNumber}.`;
+  return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
 }
